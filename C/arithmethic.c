9c73222af6b362e2b512b26e08f2f0612b800c8b
Estropia <90556292+Estropia@users.noreply.github.com>
2024-01-25 11:36:09 +0800

    Add files via upload

---
+++ C/arithmethic.c
@@ -0,0 +1,25 @@
+/*This program is a Demo for Arithmethic operations*/
+/*01/25/2024*/
+/*Created by Ferreras Mark Nielsen*/
+/*Computer Programming 1*/
+
+#include <stdio.h>
+int main(void) {
+   int sum, diff, prod, quo, mod1, mod2;
+
+   sum = 24 + 87;
+   diff = 76 - 6;
+   prod = 9 * 25;
+   quo = 20 / 5;
+   mod1 = 28 % 3;
+   mod2 = 5 % 13;
+
+   printf("Sum = %d \n", sum);
+   printf("Difference = %d \n", diff);
+   printf("Product = %d \n", prod);
+   printf("Quotient = %d \n", quo);
+   printf("Modulo 1 = %d \n", mod1);
+   printf("Modulo 2 = %d \n", mod1);
+
+   return 0;
+}
